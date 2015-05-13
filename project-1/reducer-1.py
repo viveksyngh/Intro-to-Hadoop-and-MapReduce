3efbfb22afe6adcef40f2e326fd47b1d28c04438
training <training@localhost.localdomain>
2015-05-13 15:42:27 -0400

    Mapper and Reducer for assignment 1

---
+++ project-1/reducer-1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import sys
+sales = 0.0
+oldItem = None
+for line in sys.stdin :
+	data = line.strip().split("\t")
+	if len(data) != 2 :
+		continue
+	curItem, curValue = data
+	if oldItem and curItem != oldItem :
+		print "{0}\t{1}".format(oldItem, sales)
+		oldItem = curItem
+		sales = 0.0
+	oldItem = curItem 
+	sales += float(curValue)
+if oldItem != None :
+	print "{0}\t{1}".format(oldItem, sales)
+
+
